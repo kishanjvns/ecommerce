bd4b380ec6bf1435598c7c166890cbbc48c53f90
kishanjvns <kishanjvns@hotmail.com>
2023-08-20 10:06:37 +0530

    initial commit

---
+++ centeralconfig/src/main/java/com/tech/kj/centeralconfig/CenteralconfigApplication.java
@@ -0,0 +1,13 @@
+package com.tech.kj.centeralconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenteralconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenteralconfigApplication.class, args);
+	}
+
+}
